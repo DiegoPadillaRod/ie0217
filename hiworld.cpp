c37880badd6bf5f49ca5224344fd5008a27bfb50
DiegoPadillaRod <diego.padillarodriguez@ucr.ac.cr>
2023-09-05 22:04:31 -0600

    Cambios
    Signed-off-by: DiegoPadillaRod <diego.padillarodriguez@ucr.ac.cr>

--- hiworld.cpp
+++ hiworld.cpp
@@ -2,7 +2,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+    std::cout << "Hello World!\n";
+
+    int var1 = 0;
+    int var2;
+
+    var2 = (var1 == 3) ? 15 : 27; //Operador Terneareo
+
+    std::cout << "var2: \n" << var2 << "";
+
+    return 0; 
 }
 
--- otroarchivo.cpp
+++ otroarchivo.cpp
@@ -2,7 +2,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World, desde otro archivo!";
-    return 0;
+    std::cout << "Hello World, desde otro archivo!\n";
+
+    int var1 = 45;
+    int var2 = 2;
+    int var3 = 3;
+
+    switch (var1) { // Cases
+        case 1:
+            std::cout << "Case 1!\n";
+            break;
+        case 2:
+        case 3:
+            std::cout << "Case 3!\n";
+            break;
+        
+        default:
+            std::cout << "Case Busta Rhyms!\n";
+            break;
+        }
+
+        // Void, No tiene retorno
+        // i++, se actualiza el valor
+        // double, decimal
+
+   return 0; 
 }
 
