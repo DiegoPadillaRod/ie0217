b4bd01a8d34757e08bbd6dfa32777acd1e36439d
DiegoPadillaRod <diego.padillarodriguez@ucr.ac.cr>
2023-10-27 11:55:58 -0600

    Commit final Tarea 4 cumpliendo con pep8
    Signed-off-by: DiegoPadillaRod <diego.padillarodriguez@ucr.ac.cr>

--- T4/gestion.py
+++ T4/gestion.py
@@ -87,39 +87,85 @@ class HeladoCacique:
         # Creando lista vacia
         self.__sabores = []
 
-    # Agrgando sabores a la lista
+    # Metodo que agrega sabores a la lista
     def agregar_sabor(self, sabor):
         self.__sabores.append(sabor)
 
-    # Accede a la lista e imprime los sabores registrados
+    # Metodo que accede a la lista e imprime los sabores registrados
     def mostrar_sabores(self):
         return ", ".join(self.__sabores)
+
+
+# Función externa que valida si la entrada no esta vacia
+def validarentrada(entrada):
+    return entrada.strip() != ""
+
+
+# Función externa que valida si la entrada es s o n
+def si_o_no(entrada):
+    if entrada == 's' or entrada == 'n':
+        return False
+    return True
 
 
 if __name__ == "__main__":
 
     # Creando el menu que permite interactuar con el programa
-    print("Bienvenido al sistema de gestión de Bares.")
+    print("Bienvenido al sistema de gestión de Bares.\n")
 
     # Esperando respuesta del usuario
-    nombre = input("Ingrese el nombre del restaurante: ")
+    nombre = input("Por favor, ingrese el nombre del restaurante: ")
+
+    # Ciclo while validando entrada.
+    while not validarentrada(nombre):
+
+        # Error intrada invalida
+        print("ERROR!!! El nombre no puede estar vacío. Inténtelo de nuevo.")
+
+        nombre = input("Por favor, ingrese el nombre del restaurante: ")
 
     # Esperando respuesta del usuario
     tipo_cocina = input("Ingrese el tipo de cocina: ")
 
+    # Ciclo while validando entrada.
+    while not validarentrada(tipo_cocina):
+
+        # Error intrada invalida
+        print("ERROR!!! El tipo de cocina no puede estar vacío."
+              " Inténtelo de nuevo.")
+
+        tipo_cocina = input("Ingrese el tipo de cocina: ")
+
     # Creando Objeto Restaurante
     rest = Restaurante(nombre, tipo_cocina)
 
-    print(f"Restaurante registrado con éxito:\n"
+    print("\nRestaurante registrado con éxito:\n"
           f"{rest.describe_restaurante()}")
 
     # Esperando respuesta del usuario
-    respuesta = input("¿Desea agregar un menú temático? (s/n): ")
+    respuesta = input("\n¿Desea agregar un menú temático? (s/n): ")
+
+    while si_o_no(respuesta):
+
+        # Error intrada invalida
+        print("ERROR!!! La respuesta es inválida, "
+              "debe ser si 's' o no 'n'. Inténtelo de nuevo.")
+
+        respuesta = input("¿Desea agregar un menú temático? (s/n): ")
 
     if respuesta == "s":
 
         # Esperando respuesta del usuario
-        nombre_menu = input("Ingrese el nombre del menú temático: ")
+        nombre_menu = input("\nIngrese el nombre del menú temático: ")
+
+        # Ciclo while validando entrada.
+        while not validarentrada(nombre_menu):
+
+            # Error intrada invalida
+            print("ERROR!!! El nombre del menú no puede estar vacío."
+                  "Inténtelo de nuevo.")
+
+            nombre_menu = input("Ingrese el nombre del menú temático: ")
 
         # Creando Objeto menu tematico
         menu_fin = MenusTematicos(nombre, tipo_cocina, nombre_menu)
@@ -127,24 +173,47 @@ if __name__ == "__main__":
         print(f"Menú temático registrado con éxito: "
               f"{menu_fin.soy_menu_tematico()}")
 
-        # Creabdo Objeto helado cacique
+        # Creando Objeto helado cacique
         helado_cacique = HeladoCacique()
 
         # Esperando respuesta del usuario
-        respuesta = input("¿Desea agregar un sabor de helado Cacique? (s/n): ")
+        respuesta = input("\n¿Desea agregar un sabor de helado Cacique? "
+                          "(s/n): ")
 
+        # Ciclo while validando entrada
+        while si_o_no(respuesta):
+
+            # Error intrada invalida
+            print("ERROR!!! La respuesta es inválida, "
+                  "debe ser si 's' o no 'n'. Inténtelo de nuevo.")
+
+            respuesta = input("¿Desea agregar un sabor de helado Cacique? "
+                              "(s/n): ")
+
+        # Ciclo while respuesta si.
         while respuesta == "s":
 
+            sabor = input("\nIngrese el sabor para crear su exquisitez: ")
+
+            if validarentrada(sabor):
+
+                # Agregando informacion a la lista
+                helado_cacique.agregar_sabor(sabor)
+
+                # Mostrando los sabores registrados
+                print("\nSabor agregado con éxito.!!")
+                print("Sabores disponibles: " +
+                      f"{helado_cacique.mostrar_sabores()}")
+
+            else:
+
+                # Error intrada invalida
+                print("ERROR!!! El sabor no puede estar vacío. "
+                      "Inténtelo de nuevo.")
             # Esperando respuesta del usuario
-            sabor = input("Ingrese el sabor para crear su exquisitez: ")
-
-            # Agregando informacion a la lista
-            helado_cacique.agregar_sabor(sabor)
-
-            # Mostrando los sabores registrados
-            print("Sabor agregado con éxito.")
-            print(f"Sabores disponibles: {helado_cacique.mostrar_sabores()}")
+            respuesta = input("\n¿Desea agregar otro sabor de helado Cacique? "
+                              "(s/n): ")
 
     # Despida y final del programa
-    print("Gracias por usar el sistema de gestión de bares.")
-    print(" Hasta pronto querido usuario <3 tqm!")
+    print("Gracias por usar el sistema de gestión de bares."
+          " Hasta pronto querido usuario <3 tqm!")
